72b983a3cb929553465009da90d6f628c41cb665
MICR_21 <charles.gacari@strathmore.edu>
2025-06-29 15:11:35 +0300

    changed the format so that the report for ingredients is read

--- day16/oop-coffee-machine-start/main.py
+++ day16/oop-coffee-machine-start/main.py
@@ -2,6 +2,8 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 menu = Menu()
 machine_on = True
 while machine_on:
@@ -10,30 +12,26 @@ while machine_on:
     if coffee_type == "off":
             print("Turning off the coffee machine.")
             exit()
-
     elif coffee_type == "report":
-        report = CoffeeMaker().report()
-
-    elif coffee_type == "cappuccino":
+        coffee_maker.report()
+    else:
         drink = menu.find_drink(coffee_type)
-        coffee_maker = CoffeeMaker()
-        if coffee_maker.is_resource_sufficient(drink):
-            money_machine = MoneyMachine().make_payment(drink.cost)
-            coffee_maker = CoffeeMaker().make_coffee(drink)
+        if coffee_type == "cappuccino":
+            if coffee_maker.is_resource_sufficient(drink):
+                money_machine.make_payment(drink.cost)
+                coffee_maker.make_coffee(drink)
 
 
-    elif coffee_type == "latte":
-        drink = menu.find_drink(coffee_type)
-        coffee_maker = CoffeeMaker()
-        if coffee_maker.is_resource_sufficient(drink):
-            money_machine = MoneyMachine().make_payment(drink.cost)
-            coffee_maker = CoffeeMaker().make_coffee(drink)
+        elif coffee_type == "latte":
+            drink = menu.find_drink(coffee_type)
+            if coffee_maker.is_resource_sufficient(drink):
+                money_machine.make_payment(drink.cost)
+                coffee_maker.make_coffee(drink)
 
-    elif coffee_type == "espresso":
-        drink = menu.find_drink(coffee_type)
-        coffee_maker = CoffeeMaker()
-        if coffee_maker.is_resource_sufficient(drink):
-            money_machine = MoneyMachine().make_payment(drink.cost)
-            coffee_maker = CoffeeMaker().make_coffee(drink)
+        elif coffee_type == "espresso":
+            drink = menu.find_drink(coffee_type)
+            if coffee_maker.is_resource_sufficient(drink):
+                money_machine.make_payment(drink.cost)
+                coffee_maker.make_coffee(drink)
 
 
